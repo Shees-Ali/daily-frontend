0b511556e1826ce3d3aa6ecdfa0e6fe9945016b3
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-03-26 00:03:55 +0500

    updated section above footer and created footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { ReferralsSectionComponent } from './referrals-section/referrals-section.component';
 import { ReferralCardComponent } from './referral-card/referral-card.component';
 import { UpperFooterSectionComponent } from './upper-footer-section/upper-footer-section.component';
+import { FooterSectionComponent } from './footer-section/footer-section.component';
 
 @NgModule({
   declarations: [
@@ -13,14 +14,16 @@ import { UpperFooterSectionComponent } from './upper-footer-section/upper-footer
     InfoSectionComponent,
     ReferralsSectionComponent,
     ReferralCardComponent,
-    UpperFooterSectionComponent
+    UpperFooterSectionComponent,
+    FooterSectionComponent
   ],
   imports: [CommonModule, FormsModule],
   exports: [
     HeroSectionComponent,
     InfoSectionComponent,
     ReferralsSectionComponent,
-    UpperFooterSectionComponent
+    UpperFooterSectionComponent,
+    FooterSectionComponent
   ],
 })
 export class ComponentsModule {}
