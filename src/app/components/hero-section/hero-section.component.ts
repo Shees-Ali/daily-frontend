0bc3cb04a9a9edbe54017fd8a2110998bc9d82bc
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-03-27 19:37:47 +0500

    animation + input box updated

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger, state, style, animate, transition, // ...
+} from '@angular/animations';
+
 
 @Component({
   selector: 'hero-section',
   templateUrl: './hero-section.component.html',
   styleUrls: ['./hero-section.component.scss'],
+  animations: [
+    trigger('heroSectionAnimation', [
+      transition(':enter', [style({opacity: 0}), animate('2s')]
+    )]
+    )]
 })
+
 export class HeroSectionComponent {
   email: string = '';
   name: string = '';
-
-  getStarted() {}
+  
+  
+  // const observer = new IntersectionObserver((enteries) => {
+  //   enteries.forEach((entry) => {
+  //     console.log(entry)
+  //     if (entry.isIntersecting){
+  //       entry.target.classList.add('show');
+  //     }
+  //     else {
+  //       entry.target.classList.remove('show');
+  //     }
+  //   });
+  // });
+  // const mainElement = document.querySelectorAll('.main-section');
+  // mainElement.forEach((el) => observer.observer(el));
+  
+  getStarted(){}
+  
+  show = false;
+  constructor() { }
+  get stateName() {
+    return this.show ? 'show' : 'hide'
+  }
+  toggle() {
+    this.show = !this.show;
+  }
 }
