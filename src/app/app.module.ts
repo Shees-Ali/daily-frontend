88f3783bc21b78af0a078d4ea870600013183dd0
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-03-28 17:34:46 +0500

    misc updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentsModule } from './components/components.module';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { GetfeaturedFormComponent } from './components/getfeatured-form/getfeatured-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutSectionComponent, GetfeaturedFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
