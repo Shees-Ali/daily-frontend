dfdcfe5b3c443debe742d4583d710204e61975a1
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-05-26 17:43:44 +0500

    responsive homepage completed

--- src/app/components/description-section/description-section.component.ts
+++ src/app/components/description-section/description-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'description-section',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./description-section.component.scss']
 })
 export class DescriptionSectionComponent {
+  
+  ismedium:boolean = false;
+  screenWidth: number = 0;
 
+  constructor(){
+    this.onResize();
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    this.screenWidth = window.innerWidth;
+    this.checksize();
+  }
+
+  checksize(){
+    if (this.screenWidth <= 960 && this.screenWidth >=600){
+      this.ismedium = true;
+    }
+    else if (this.screenWidth < 460){
+      this.ismedium = true;
+    }
+    else{
+      this.ismedium = false;
+    }
+  }
 }
